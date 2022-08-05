40a8c647b54e79d281a7a2e1e98da3e83234115e
Gerardo Bautista Castañeda <gerarchicharo37@gmail.com>
2022-08-05 13:35:55 -0500

    Initial Commit

---
+++ Citas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Citas
+//
+//  Created by user216116 on 05/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
